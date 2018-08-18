581d107a877c8bc8c9134ca4841759f41370fd58
Kevin Chen <kevchen2@gmail.com>
2018-08-18 14:11:25 -0700

    Initial Commit

---
+++ HouseSearchScript/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HouseSearchScript
+//
+//  Created by Kirby on 8/18/18.
+//  Copyright © 2018 Kirby. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
